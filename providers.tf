e069d06d0d9238f90e8ee1a7fca2a722796e3cdb
prakdeep <rpkcknc@gmail.com>
2026-04-24 14:20:55 +0530

    adding new pipeline filesdgdfgg

--- providers.tf
+++ providers.tf
@@ -8,14 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
+  features {}
 
-  subscription_id = "79bdf282-66fa-4bdc-b5dc-5f029b9647de"
-
-  features {
-    
-
-  }
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 
   use_cli = false
-
 }
--- variables.tf
+++ variables.tf
@@ -12,6 +12,23 @@ variable "rg" {
 }
 
 
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+}
+
 variable "nicv" {
 
   type = map(object({
