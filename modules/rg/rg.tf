4cd83a05a8257eac32d791a62913401eaa0c7b5b
prakdeep <rpkcknc@gmail.com>
2026-04-26 19:33:16 +0530

    adding new pipeline filedldkdsdfdfsdsdsdsdklsdsdfsfsd

--- modules/rg/rg.tf
+++ modules/rg/rg.tf
@@ -7,16 +7,3 @@ resource "azurerm_resource_group" "testrg" {
 
 }
 
-resource "azurerm_network_interface" "name" {
-
-  location            = "southwestus"
-  resource_group_name = "sdfsdf"
-  name                = "stkl"
-
-  ip_configuration {
-    name = "sedesd"
-
-    private_ip_address_allocation = "Static"
-
-  }
-}
